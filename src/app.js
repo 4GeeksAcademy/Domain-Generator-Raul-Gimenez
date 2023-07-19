803fbe0a09c8a55f979ae0be5affae7155787bb9
Raul Gimenez Murga <136567985+Raauul1996@users.noreply.github.com>
2023-07-19 18:34:15 +0000

    Crate Domain Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generateAllCombinations (){
+  const startPart = ["the", "our", "yours"];
+  const middlePart = ["cars", "motorbike", "airplane"];
+  const endPart = ["collection", "garage", "museum"];
+  let result = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  for (let i = 0; i < startPart.length; i++){
+    for (let j = 0; j < middlePart.length; j++){
+      for (let p = 0; p < endPart.length; p++){
+        result.push(`${startPart[i]} ${middlePart[j]} ${endPart[p]}`);
+      }
+    }
+  }
+  return result;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log(generateAllCombinations().join(`\n`));
