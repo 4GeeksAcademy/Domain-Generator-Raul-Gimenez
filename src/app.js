c92974a570bab0d707ec524b25b3b57560f16443
Raul Gimenez Murga <136567985+Raauul1996@users.noreply.github.com>
2023-07-22 18:39:19 +0000

    Add extension

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
-function generateAllCombinations (){
+function generateAllCombinations() {
   const startPart = ["the", "our", "yours"];
   const middlePart = ["cars", "motorbike", "airplane"];
   const endPart = ["collection", "garage", "museum"];
+  const extension = [".com", ".es", ".us"];
   let result = [];
 
-  for (let i = 0; i < startPart.length; i++){
-    for (let j = 0; j < middlePart.length; j++){
-      for (let p = 0; p < endPart.length; p++){
+  for (let i = 0; i < startPart.length; i++) {
+    for (let j = 0; j < middlePart.length; j++) {
+      for (let p = 0; p < endPart.length; p++) {
+        for (let r = 0; r < extension.length; r++){
         result.push(`${startPart[i]} ${middlePart[j]} ${endPart[p]}`);
+        }
       }
     }
   }
